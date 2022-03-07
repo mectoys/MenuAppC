867f7914304b2880489b8224b89abd6debd669ab
mectoys <mectoys2013@gmail.com>
2022-03-06 22:03:24 -0500

    Creación Mainform , form pedido

---
+++ MenuAppsC/MenuAppsC/Pantallas/frm_pedido.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MenuAppsC.Pantallas
+{
+    public partial class frm_pedido : Form
+    {
+        public frm_pedido()
+        {
+            InitializeComponent();
+        }
+    }
+}
